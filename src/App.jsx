35dacf3ed782ea438ca309f47e2c4f54d9aa0061
pvy-it-tdh <phucvy107@gmail.com>
2024-05-12 14:14:54 +0700

    chỉnh sửa sương sương

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Nav from "./Layouts/Header";
 import Home from "./Components/Home";
 import Activities from "./Components/Activities";
 import Members from "./Components/Activities";
+
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Nav />
-        <Home/>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/activities" element={<Activities/>} />
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,15 +1,12 @@
 import Footer from "../Layouts/Footer";
 import Nav from "../Layouts/Header";
-
 import Banner from "./Banner";
-import Members from "./Member";
 
 const Home = () => {
   return (
     <div >
     <Nav/>
     <Banner/>
-    <Members/>
     <Footer/>
     </div>
   );
