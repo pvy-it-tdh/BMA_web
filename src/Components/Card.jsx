c4052f336cc0e2765d4a9373103b6729fe7325e2
pvy-it-tdh <phucvy107@gmail.com>
2024-05-16 16:53:40 +0700

    add animation

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,17 +1,27 @@
-
+import { motion } from "framer-motion";
 import "./Card.css";
 
 function Card(props) {
-  const { imageUrl,title, description } = props;
+  const { imageUrl, title, description } = props;
 
   return (
-    <div className="card">
+    <motion.div
+      className="card"
+      whileHover={{
+        scale: 1.05,
+        rotateY: 10,
+        boxShadow: "0 12px 24px rgba(0, 0, 0, 0.2)",
+      }}
+      initial={{ opacity: 0, rotateY: -45, z: -100 }}
+      animate={{ opacity: 1, rotateY: 0, z: 0 }}
+      transition={{ type: "spring", stiffness: 100, damping: 10 }}
+    >
       <img src={imageUrl} alt={title} className="card-image" />
       <div className="card-content">
         <h2 className="card-title">{title}</h2>
         <p className="card-description">{description}</p>
       </div>
-    </div>
+    </motion.div>
   );
 }
 
