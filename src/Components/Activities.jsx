3f0d2a055ec10eaa3cbb8fb2332bda603ddf4b99
pvy-it-tdh <phucvy107@gmail.com>
2024-06-17 16:57:57 +0700

    up Activities

--- src/Components/Activities.jsx
+++ src/Components/Activities.jsx
@@ -1,9 +1,19 @@
+import Card from "./Card";
+import Biggame from "./Image/Biggame.jpg";
+import "./Activities.css";
 const Activities = () => {
   return (
-    <div className="bg-zinc-900">
-      <h1 className="text-[30px] font-semibold mb-2 text-center text-cyan-400">
+    <div className="activities-container">
+      <h1 className="activities-title">
         Một số hoạt động truyền thống của CLB
       </h1>
+      <div className="activities-group">
+        <Card
+          imageUrl={Biggame}
+          title="Biggame 2023"
+          description="Là 1 sự kiện thường niên và lớn nhất của câu lạc bộ"
+        />
+      </div>
     </div>
   );
 };
