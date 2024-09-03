2d59ab3fab916b531cb2111250298f2384abb050
pvy-it-tdh <phucvy107@gmail.com>
2024-09-03 17:12:24 +0700

    fix Card css

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,13 +3,7 @@ import "../styles/Card.css";
 function Card({ imageUrl, title, description }) {
   return (
     <div className="card">
-      <div
-        className="card-image"
-        style={{
-          backgroundImage: `url(${imageUrl})`,
-          backgroundSize: "cover",
-        }}
-      ></div>
+      <img className="card-image" src={imageUrl} alt={title} />
       <div className="card-content">
         <h2 className="card-title">{title}</h2>
         <p className="card-body">{description}</p>
